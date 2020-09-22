84fef0b8037d32c573f60e683c98f2b7399691e7
bellus <lushunde321@163.com>
2020-09-22 23:56:03 +0800

    first

---
+++ src/main/java/com/lushunde/maven/profile/ApplicationMain.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright © 2020, lushunde or www.lushunde.com site. All rights reserved.
+ * @package maven-profile com.lushunde.maven.profile 
+ * @author bellus
+ * @node 
+ */
+package com.lushunde.maven.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @node
+ * @package maven-profile com.lushunde.maven.profile
+ * @version 1.0
+ * @date 2020年9月22日
+ * @author bellus
+ * @since JDK1.8
+ */
+@SpringBootApplication
+public class ApplicationMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationMain.class, args);
+	}
+}
